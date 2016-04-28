ad299fce4ebaf182eefe2e56717d88b8092dd5c3
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-04-27 21:06:09 -0700

    Added files via upload

---
+++ Dice.java
@@ -0,0 +1,9 @@
+package week11lab4;
+
+public class Dice {
+    
+    public static int OneThrow(int roll){
+        roll = 1 + (int) (Math.random() * 6);
+        return roll;
+    }
+}
---
+++ Week11Lab4.java
@@ -0,0 +1,11 @@
+package week11lab4;
+
+public class Week11Lab4 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Dice.OneThrow(1));
+        
+    }
+    
+}
