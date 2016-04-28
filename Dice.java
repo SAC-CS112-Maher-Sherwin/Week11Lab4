33a489da025edf01bbab46d5971c0eaf0ff78321
SAC-CS112-Maher-Sherwin <sherwin.maher@gmail.com>
2016-04-27 21:08:28 -0700

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -2,8 +2,8 @@ package week11lab4;
 
 public class Dice {
     
-    public static int OneThrow(int roll){
-        roll = 1 + (int) (Math.random() * 6);
+    public static int OneThrow(){
+        int roll = 1 + (int) (Math.random() * 6);
         return roll;
     }
 }
